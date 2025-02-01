25c325d9818ca0804025593f857f23fe167d0639
pedro.mendes <pedro.mendes@g.globo>
2025-02-01 19:05:25 -0300

    feat: add bubbletea to improve tui

--- cmd/gotop/main.go
+++ cmd/gotop/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/pedrolopesme/books/internal/proclist"
-	"github.com/pedrolopesme/books/pkg/utils"
+	"github.com/pedrolopesme/books/internal/tui"
 )
 
 func main() {
-	procs, err := proclist.GetProcesses()
-	if err != nil {
+
+	if err := tui.StartTUI(); err != nil {
 		log.Fatal(err)
 	}
 
-	for _, p := range procs {
-		fmt.Printf("PID: %d, Name: %s | CPU Usage: %.2f%% | Memory: %s \n", p.Pid, p.Name, p.CPUPerc, utils.FormatBytes(p.Memory))
-	}
 }
